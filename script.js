1459ce820f5e21994765e784caf619f97d86af85
Samaltmane <samforeverx1@gmail.com>
2025-08-06 11:53:12 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+// Using ipapi.co as an example
+fetch("https://ipapi.co/json/")
+  .then(response => response.json())
+  .then(data => {
+    const info = `
+      <p><strong>IP:</strong> ${data.ip}</p>
+      <p><strong>City:</strong> ${data.city}</p>
+      <p><strong>Region:</strong> ${data.region}</p>
+      <p><strong>Country:</strong> ${data.country_name}</p>
+      <p><strong>ISP:</strong> ${data.org}</p>
+    `;
+    document.getElementById("ip-info").innerHTML = info;
+  })
+  .catch(error => {
+    document.getElementById("ip-info").innerHTML = "<p>Error loading IP info.</p>";
+    console.error("IP fetch error:", error);
+  });
